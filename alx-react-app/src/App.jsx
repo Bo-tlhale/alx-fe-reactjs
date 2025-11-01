8c44e758ef3d69d091f5ad1ac3aa1d5e5a09b265
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-01 11:50:19 +0200

    Created main components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,12 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <WelcomeMessage />
+	  
+	  <Header />
+	  <MainContent />
+	  <Footer />
     </>
   )
 }
