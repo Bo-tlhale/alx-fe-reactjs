338322f3619330ef736e3ad211690cd257a7c82e
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-08 11:37:35 +0200

    Changed the color of the user profile heading for the checker

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile(props) {
 	return(
 		<>
 			<div style={{backgroundColor: 'lavenderblush', color: 'black', height: '175px', width: '150px', border: '2px solid purple', padding: '10px', margin: '0 auto'}}>
-				<h2 style={{color: 'purple', fontSize: '25px'}}>{props.name}</h2>
+				<h2 style={{color: 'blue', fontSize: '25px'}}>{props.name}</h2>
 				<p style={{textAlign: 'left', padding: '0.5px', fontWeight: 'bold'}}>Age: <span style={{fontWeight: 'normal'}}>{props.age} </span></p>
 				<p style={{textAlign: 'left', padding: '0.5px', fontWeight: 'bold'}}>Bio: <span style={{fontWeight: 'normal'}}>{props.bio} </span></p>
 			</div>
