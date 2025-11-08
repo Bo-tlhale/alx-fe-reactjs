ef4dc14be84a1cc8f16e7038fe7880d22b195c8f
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-08 11:30:58 +0200

    Created a new directory using inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+	return (
+		<footer style={{backgroundColor: 'lavenderblush', width: '100%', position: 'fixed', bottom: '0', left: '0', right: '0'}}>
+		  <p style={{color: 'darkgray', textAlign: 'center'}}>© 2023 City Lovers</p>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+	return (
+		<main style={{margin: '0 auto', color: 'black', height: '50px', width: '100%'}}>
+		  <p>I love to visit New York, Paris, and Tokyo.</p>
+		</main>
+	);
+}
+
+export default MainContent;
