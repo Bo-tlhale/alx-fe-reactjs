6c0db8a18885a081b3bf002b24a21a5980e3937e
Botlhale Mpatlanyane <botlhale.btm@gmail.com>
2025-11-14 13:24:42 +0200

    Implemented logic for checker

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,6 +1,9 @@
+import { useNavigate } from 'react-router-dom'
 import useRecipeStore from './recipeStore'
 
 const DeleteRecipeButton = ({recipeId}) => {
+    let navigate = useNavigate('/recipes');
+
     const recipe = useRecipeStore(state => state.deleteRecipe)
     
     const handleDelete = (event) => {
@@ -10,7 +13,7 @@ const DeleteRecipeButton = ({recipeId}) => {
     
     return(
         <div>
-            <button onClick={handleDelete}>Delete Recipe</button>
+            <button onClick={handleDelete && navigate}>Delete Recipe</button>
         </div>
     )
 }
--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link} from 'react-router-dom'
 import useRecipeStore from './recipeStore'
 import DeleteRecipeButton from './DeleteRecipeButton'
 
